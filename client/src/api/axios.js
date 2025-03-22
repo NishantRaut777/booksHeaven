ffac47a9fbcaae512c1fc204dfb718aef10985a8
Nishant <nishraut7@gmail.com>
2025-03-22 09:23:41 +0530

    backend url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
+// const axiosInstance = axios.create({
+//     baseURL: process.env.NODE_ENV === 'development'
+//         ? `${process.env.REACT_APP_BACKEND_URL}`
+//         : 'devurl',
+//     withCredentials: true,
+// });
+
 const axiosInstance = axios.create({
-    baseURL: process.env.NODE_ENV === 'development'
-        ? `${process.env.REACT_APP_BACKEND_URL}`
-        : 'devurl',
+    baseURL: 'https://booksheaven.onrender.com',
     withCredentials: true,
 });
 

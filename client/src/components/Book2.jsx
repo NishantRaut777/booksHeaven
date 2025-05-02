f88b99183df2d89ca281744753624a33a5215745
Nishant <nishraut7@gmail.com>
2025-05-02 21:10:34 +0530

    fix(button color hover fix)

--- client/src/components/Book2.jsx
+++ client/src/components/Book2.jsx
@@ -86,7 +86,7 @@ const Book2 = ({
       <div className="add-to-cart-div flex flex-row justify-between bg-gray-100 ">
         <span
           onClick={handleAddToCart}
-          className="p-3 m-2 cursor-pointer bg-purple-200 rounded-sm transition-all duration-300 ease-in-out hover:bg-orange-400 "
+          className="p-3 m-2 cursor-pointer bg-purple-200 rounded-sm transition-all duration-300 ease-in-out hover:bg-orange-400 active:bg-purple-200"
         >
           Add to cart
         </span>
